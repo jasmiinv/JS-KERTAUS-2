61d4244af3a0f8aca94e6468f9a6d2381ad4c3ae
jade <jasmiinv@metropolia.fi>
2023-08-25 12:06:52 +0300

    koodia

--- T4/t4.js
+++ T4/t4.js
@@ -1,2 +1,17 @@
 'use strict';
 
+function sortArray(numbers) {
+  // Create a new array from the original to avoid mutating it
+  const newArray = [...numbers];
+
+  // Sort the new array in ascending order
+  newArray.sort((a, b) => a - b);
+
+  // Return the sorted array
+  return newArray;
+}
+const numbers = [30, 2, 5, 200];
+console.log('Alkuperäinen jono:', numbers);
+
+const sortedNumbers = sortArray(numbers);
+console.log('Järjestetty jono:', sortedNumbers);
